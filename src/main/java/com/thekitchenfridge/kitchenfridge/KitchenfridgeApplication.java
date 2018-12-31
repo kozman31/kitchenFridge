a1c5930eb4379c3e26f1f71925de0f91724cd41e
Elliott Kozulak <kozman31@gmail.com>
2018-12-30 22:44:43 -0500

    First commit

---
+++ src/main/java/com/thekitchenfridge/kitchenfridge/KitchenfridgeApplication.java
@@ -0,0 +1,14 @@
+package com.thekitchenfridge.kitchenfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenfridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenfridgeApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/thekitchenfridge/kitchenfridge/KitchenfridgeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.thekitchenfridge.kitchenfridge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KitchenfridgeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
