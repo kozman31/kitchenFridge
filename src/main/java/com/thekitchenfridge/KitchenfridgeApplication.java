ae82f69080148c331c5f592c10c433b3ede5735c
ElliottK <kozman31@gmail.com>
2020-01-27 19:44:22 -0500

    removed annotations

--- src/main/java/com/thekitchenfridge/KitchenfridgeApplication.java
+++ src/main/java/com/thekitchenfridge/KitchenfridgeApplication.java
@@ -5,30 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 public class KitchenfridgeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KitchenfridgeApplication.class, args);
 	}
-
-//	@Bean
-//	private CommandLineRunner initDatabase(UserDetailsServiceImpl userDetailsService) {
-//		Authority authority = new Authority();
-//		authority.setName("admin");
-//		Set<Authority> authSet = new HashSet<>();
-//		authSet.add(authority);
-//		Role.builder().roleName("ADMIN").authorities(authSet).build();
-//		UserProfileDto user = new UserProfileDto("Admin", "secretPw", 1L);
-//
-//		};
-//	}
 }
 
-/*
-private String username;
-private String password;
-private Role role;
-private String firstName;
-private String lastName;
-private String email;*/
