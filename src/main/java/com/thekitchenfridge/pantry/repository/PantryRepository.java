50a85e139fdfd34d061906ef0acbc441972cffa1
ElliottK <kozman31@gmail.com>
2020-01-27 19:57:52 -0500

    added repository annotations

--- src/main/java/com/thekitchenfridge/pantry/repository/PantryRepository.java
+++ src/main/java/com/thekitchenfridge/pantry/repository/PantryRepository.java
@@ -2,6 +2,8 @@ package com.thekitchenfridge.pantry.repository;
 
 import com.thekitchenfridge.pantry.entity.PantryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PantryRepository extends JpaRepository<PantryItem, Long> {
 }
