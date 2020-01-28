b16509417c1848c3ca5949b885d68d2940482be5
ElliottK <kozman31@gmail.com>
2020-01-27 20:33:48 -0500

    fixed sql column names

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,8 +8,8 @@ WHERE NOT EXISTS (SELECT * FROM roles WHERE role_name='ROLE_USER');
 
 INSERT INTO authorities (authority_name, authority_id)
 SELECT 'admin', 1
-WHERE NOT EXISTS (SELECT * FROM roles WHERE authority_name='admin');
+WHERE NOT EXISTS (SELECT * FROM authorities WHERE authority_name='admin');
 
 INSERT INTO authorities (authority_name, authority_id)
 SELECT 'user', 2
-WHERE NOT EXISTS (SELECT * FROM roles WHERE authority_name='user');
+WHERE NOT EXISTS (SELECT * FROM authorities WHERE authority_name='user');
