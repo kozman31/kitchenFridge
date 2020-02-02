295ac2a7b4affd647132d0abb27bf9e44903f893
ElliottK <kozman31@gmail.com>
2020-02-01 22:32:15 -0500

    updated cors

--- src/main/java/com/thekitchenfridge/security/CustomSecurityConfig.java
+++ src/main/java/com/thekitchenfridge/security/CustomSecurityConfig.java
@@ -71,7 +71,7 @@ public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://localhost:3000", "https://ekoz.dev"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
